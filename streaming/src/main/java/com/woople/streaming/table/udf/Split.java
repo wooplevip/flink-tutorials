17effa85eab43b617a55321d578b0f73c8f6dbf8
wooplevip <woople@foxmail.com>
2020-04-01 10:17:06 +0800

    Update demo

--- streaming/src/main/java/com/woople/streaming/table/udf/Split.java
+++ streaming/src/main/java/com/woople/streaming/table/udf/Split.java
@@ -1,4 +1,19 @@
 package com.woople.streaming.table.udf;
 
-public class Split {
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.table.functions.TableFunction;
+
+public class Split extends TableFunction<Tuple2<String, Integer>> {
+    private String separator = " ";
+
+    public Split(String separator) {
+        this.separator = separator;
+    }
+
+    public void eval(String str) {
+        for (String s : str.split(separator)) {
+            // use collect(...) to emit a row
+            collect(new Tuple2<String, Integer>(s, s.length()));
+        }
+    }
 }
--- streaming/src/main/java/com/woople/streaming/table/udf/UDFDemo.java
+++ streaming/src/main/java/com/woople/streaming/table/udf/UDFDemo.java
@@ -1,15 +1,16 @@
-package com.woople.streaming.table;
+package com.woople.streaming.table.udf;
 
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
 
-public class AppendOnlyExample {
+public class UDFDemo {
 	public static void main(String[] args) throws Exception {
 		EnvironmentSettings settings = EnvironmentSettings.newInstance().inStreamingMode().useBlinkPlanner().build();
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -17,8 +18,8 @@ public class AppendOnlyExample {
 		env.setParallelism(1);
 
 		DataStream<Tuple2<String, String>> data = env.fromElements(
-				new Tuple2<>("Mary", "./home"),
-				new Tuple2<>("Bob", "./cart"),
+				new Tuple2<>("Mary", "./home#./opt"),
+				new Tuple2<>("Bob", "./cart#./tmp#./var"),
 				new Tuple2<>("Mary", "./prod?id=1"),
 				new Tuple2<>("Liz", "./home"),
 				new Tuple2<>("Bob", "./prod?id=3")
@@ -26,10 +27,19 @@ public class AppendOnlyExample {
 
 		Table clicksTable = tEnv.fromDataStream(data, "user,url");
 
-		tEnv.registerTable("clicks", clicksTable);
-		Table rTable = tEnv.sqlQuery("select user,url from clicks where user='Mary'");
+		tEnv.createTemporaryView("clicks", clicksTable);
+		tEnv.registerFunction("split", new Split("#"));
 
-		DataStream ds = tEnv.toAppendStream(rTable, TypeInformation.of(new TypeHint<Tuple2<String, String>>(){}));
+//		clicksTable.joinLateral("split(url) as (path, length)")
+//				.select("url, path, length");
+//		clicksTable.leftOuterJoinLateral("split(url) as (path, length)")
+//				.select("path, length");
+
+		//Table rTable = tEnv.sqlQuery("SELECT url, path, length FROM clicks, LATERAL TABLE(split(url)) as T(path, length)");
+
+		Table rTable = tEnv.sqlQuery("SELECT url, path, length FROM clicks LEFT JOIN LATERAL TABLE(split(url)) as T(path, length) ON TRUE");
+
+		DataStream ds = tEnv.toAppendStream(rTable, TypeInformation.of(new TypeHint<Tuple3<String, String, Long>>(){}));
 		ds.print();
 		env.execute();
 	}
